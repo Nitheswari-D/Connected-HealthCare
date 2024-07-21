ddd30c894ccb322a2d0a86448dbf6c667e124553
Nitheswari.D <103818470+Nitheswari-D@users.noreply.github.com>
2024-07-22 00:18:36 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-const navMenu = document.querySelector('nav ul');
-const navToggle = document.querySelector('.nav-toggle');
-
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
